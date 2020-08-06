65434712d5a381fa15ad438f55ae2221834519c4
vagrant <vagrant@localhost.localdomain>
2020-08-06 05:01:21 +0000

    [Add] show

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -12,6 +12,11 @@ class TodolistsController < ApplicationController
   def index
   	@lists = List.all
   end
+
+  def show
+  	@list = List.find(params[:id])
+  end
+
   private
   def list_params
   	params.require(:list).permit(:title, :body)
