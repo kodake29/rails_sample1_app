15f2e5447811e343e7a0170e1d4f4f83cbcece29
vagrant <vagrant@localhost.localdomain>
2020-08-06 06:46:43 +0000

    [Add] 4-9 delete

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,7 @@ get 'todolists/:id' => 'todolists#show', as: 'todolist'
 get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
 
 patch 'todolists/:id' =>'todolists#update', as: 'update_todolist'
+
+delete 'todolists/:id' =>'todolists#destroy', as: 'destroy_todolist'
+
 end
